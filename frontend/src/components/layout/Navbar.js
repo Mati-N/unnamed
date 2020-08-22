54bdafc3f637bf3cd4de943726400c3995fa3710
Mati <mathiaskassaye8@gmail.com>
2020-08-22 14:10:33 +0300

    yo

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -27,9 +27,17 @@ function Navbar() {
         <Link to="/" className="navbar-brand">
           Geel
         </Link>
-        <button className="fa fa-bars" onClick={showMenu}>
-          ///
-        </button>
+        <svg
+          viewBox="0 0 100 80"
+          width="40"
+          height="40"
+          className="fa fa-bars"
+          onClick={showMenu}
+        >
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
       </div>
 
       <ul
