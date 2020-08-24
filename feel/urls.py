5b242e3ef3713b868f5a48f36ba86fc3a6fc0ddd
Mati <mathiaskassaye8@gmail.com>
2020-08-24 09:24:21 +0300

    cleaning up

--- feel/urls.py
+++ feel/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from api import views
 from graphql_jwt.decorators import jwt_cookie
@@ -7,6 +7,7 @@ from graphene_django.views import GraphQLView
 from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path("/", include("frontend.urls")),
     path("api/", jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=False)))),
     path('admin/', admin.site.urls),
 ]
