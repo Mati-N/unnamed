77ea728b93f9ec087d6a6f9e1a4389a34ddc6b4e
Mati <mathiaskassaye8@gmail.com>
2020-08-26 10:33:21 +0300

    fixing

--- feel/settings.py
+++ feel/settings.py
@@ -52,9 +52,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
