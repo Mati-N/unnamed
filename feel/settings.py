bcdf28577e5d9eac9cbb4fd2a45f43eb16c8d472
Mati <mathiaskassaye8@gmail.com>
2020-08-26 11:24:43 +0300

    fixing

--- feel/settings.py
+++ feel/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'graphene_django',
     'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
 ]
