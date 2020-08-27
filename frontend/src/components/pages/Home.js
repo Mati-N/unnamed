9d0b4c5188d3981eec654505ec256075d1bf28e9
Mati <mathiaskassaye8@gmail.com>
2020-08-27 20:51:37 +0300

    fixing

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -5,7 +5,7 @@ import { useQuery } from "@apollo/client";
 import { Waypoint } from "react-waypoint";
 import { ImpulseSpinner as Spinner } from "react-spinners-kit";
 const Offline = lazy(() => import("./Offline"));
-const PostItem = lazy(() => import("../post/PostItem"));
+//const PostItem = lazy(() => import("../post/PostItem"));
 
 function Home() {
   const { loading, data, error, fetchMore, refetch } = useQuery(GET_POSTS);
@@ -73,7 +73,7 @@ function Home() {
             user_id={node.user.id}
             username={node.user.username}
           />*/
-          }(<>{node.id}</>))
+          }("beautiful"))
         )}
         <Waypoint onEnter={more}>
           <div className="spinner">{spin && <Spinner size={40} />}</div>
