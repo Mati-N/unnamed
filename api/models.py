7685e8681751dfc9076517792c1d3bffa49fffa2
Mati <mathiaskassaye8@gmail.com>
2020-08-28 20:16:19 +0300

    fixing

--- api/models.py
+++ api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from datetime import datetime
 import pytz
 from django.contrib.auth.models import User
-
 utc=pytz.UTC
     
 # Post model
@@ -12,16 +11,16 @@ class Post(models.Model):
     text = models.CharField(max_length=5500)
     commentCount = models.IntegerField(default=0)
     creation = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
+                                               
+    def __str__(self):                                
         return f"Post {self.id}: {self.title}: created {self.before()} ago"
-
-# Comment model
-class Comment(models.Model):
+                                                      
+# Comment model                                       
+class Comment(models.Model):                             
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    creation = models.DataTimeField(auto_now_add=True)
+    creation = models.DateTimeField(default=datetime.now())
 
     def __str__(self):
         return f"Comment {self.id} on Post"
