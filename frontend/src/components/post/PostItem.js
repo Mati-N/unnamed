56354a587e190a2d472b8d7ecbce57803995be81
Mati <mathiaskassaye8@gmail.com>
2020-08-29 16:30:18 +0300

    adding support for adding and viewing comments

--- frontend/src/components/post/PostItem.js
+++ frontend/src/components/post/PostItem.js
@@ -118,7 +118,7 @@ const PostItem = ({
         if (cache) {
           let { postComments } = cache.readQuery({
             query: GET_POST,
-            variables: { id: parseInt(id) },
+            variables: { id: parseInt(id), comment },
           });
           if (postComments) {
             const newData = {
