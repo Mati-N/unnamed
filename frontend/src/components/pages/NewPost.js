6dcb2546dbf0cd1aca620fc0706382129260ba1b
Mati <mathiaskassaye8@gmail.com>
2020-08-30 11:09:42 +0300

    adding comment support

--- frontend/src/components/pages/NewPost.js
+++ frontend/src/components/pages/NewPost.js
@@ -2,7 +2,8 @@ import React, { useState, useContext, useEffect, lazy } from "react";
 import { useMutation } from "@apollo/client";
 import { CREATE_POST, GET_POSTS } from "../../Queries";
 import AlertContext from "../../context/alert/AlertContext";
-const { Redirect } = lazy(() => import("react-router-dom"));
+//const { Redirect } = lazy(() => import("react-router-dom"));
+import { Redirect } from "react-router-dom";
 
 const NewPost = () => {
   const [state, setState] = useState({ title: "", content: "" });
