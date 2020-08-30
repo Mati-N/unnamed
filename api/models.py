ba451d889f4817b0750e8c2981a54cb613188887
Mati <mathiaskassaye8@gmail.com>
2020-08-30 18:12:57 +0300

    Modified User model in app to store more information

--- api/models.py
+++ api/models.py
@@ -12,6 +12,8 @@ __all__ = ['User', 'Post', 'Comment', 'Like', 'Following']
 
 class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True, null=True)
+    USERNAME_FIELD = "username"
+    PASSWORD_FIRLD = "password"
 
     @property
     def follower_count(self):
