5aec79b832af433fe00f221ea969b1dc1b69dc38
Mati <mathiaskassaye8@gmail.com>
2020-08-31 13:36:01 +0300

    modifiying models

---
+++ staticfiles/frontend/2.main.js
@@ -0,0 +1,6 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"./src/components/pages/Offline.js":
+/*!*****************************************!*\
+  !*** ./src/components/pages/Offline.js ***!
+  \*****************************************/
+/*! exports provided: default */function(e,n,a){"use strict";a.r(n);var l=a(/*! react */"./node_modules/react/index.js"),t=a.n(l);n.default=function(){return t.a.createElement(t.a.Fragment,null,t.a.createElement("h1",null," It seems like you are offline ")," ",t.a.createElement("small",null," Try refreshing ")," ")}}}]);
+//# sourceMappingURL=2.main.js.map
