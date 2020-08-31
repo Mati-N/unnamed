d4c768a86072f84fb2e09bbe2fda5e2ff0809872
Mati <mathiaskassaye8@gmail.com>
2020-08-31 12:52:51 +0300

    modifiying models

--- feel/settings.py
+++ feel/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6z7orf7**r)yisjf5@bkwe$#_s1n^&-%&$l!+2#n=@7ulknv_%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['selamselam.herokuapp.com', 'localhost']
 
