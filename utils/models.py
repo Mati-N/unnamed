a0a2924cf364b2c39fcbc7f55203cb2c0dceff90
Mati <mathiaskassaye8@gmail.com>
2020-08-31 12:00:45 +0300

    modifiying models

--- utils/models.py
+++ utils/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 __all__ = ['AutoTimeStamped']
 
-
+# The name describes it
 class AutoTimeStamped(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
