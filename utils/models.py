ec2fdcd731dc5fda6cb6deb235a00c452ad5913b
Mati <mathiaskassaye8@gmail.com>
2020-08-31 12:07:33 +0300

    modifiying models

--- utils/models.py
+++ utils/models.py
@@ -6,6 +6,7 @@ __all__ = ['AutoTimeStamped']
 class AutoTimeStamped(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    l = models.IntegerField(default=10)
 
     class Meta:
         abstract = True
