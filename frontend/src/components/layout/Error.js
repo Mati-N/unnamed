95f065d4f6c157b2b75038a7bfee8c46038e1647
Mati <mathiaskassaye8@gmail.com>
2020-09-02 13:03:37 +0300

    Fixing(and recovering deleted files) problems caused by accidentally deleting files

--- frontend/src/components/layout/Error.js
+++ frontend/src/components/layout/Error.js
@@ -3,11 +3,21 @@ import React from "react";
 function Error({ error, componentStack, resetErrorBoundary }) {
   return (
     <div role="alert">
-      <p>Something went wrong:</p>
+      <p
+        style={{
+          width: "40%",
+          margin: "auto",
+          padding: "8vh",
+        }}
+      >
+        Something went wrong:
+      </p>
+      {/*
       <pre>{componentStack}</pre>
       <button className="btn-teal btn" onClick={resetErrorBoundary}>
         Try again
       </button>
+      */}
     </div>
   );
 }
