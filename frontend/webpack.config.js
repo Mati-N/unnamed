a17cb5ac741ee553d5668bdce462ada65036f441
Mati <mathiaskassaye8@gmail.com>
2020-09-03 16:01:54 +0300

    Fixing(and recovering deleted files) problems caused by accidentally deleting files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,33 +1,40 @@
-module.exports = {
-  entry: {
-    main: ["core-js/stable", "regenerator-runtime/runtime", "src/main.js"],
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+module.exports = (env, argv) => {
+  let config = {
+    entry: {
+      main: ["core-js/stable", "regenerator-runtime/runtime", "src/main.js"],
+    },
+    module: {
+      rules: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
-    ],
-  },
-  output: {
-    publicPath: "/static/frontend/",
-  },
-  //devtool: "source-map",
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/,
-  },
-  optimization: {
-    minimize: true,
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        },
+      ],
+    },
+    output: {
+      publicPath: "/static/frontend/",
+    },
+    devtool: "source-map",
+    watch: true,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    optimization: {
+      minimize: true,
+    }
   }
+
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+  return config;
 };
--- static/frontend/1.main.js
+++ static/frontend/1.main.js
@@ -1,1 +1,6 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{96:function(e,n,a){"use strict";a.r(n);var l=a(2),t=a.n(l);n.default=function(){return t.a.createElement(t.a.Fragment,null,t.a.createElement("h1",null," It seems like you are offline ")," ",t.a.createElement("small",null," Try refreshing ")," ")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"./src/components/post/Posts.js":
+/*!**************************************!*\
+  !*** ./src/components/post/Posts.js ***!
+  \**************************************/
+/*! exports provided: default */function(e,n,t){"use strict";t.r(n);var r=t(/*! react */"./node_modules/react/index.js"),s=t.n(r);function o(){return(o=Object.assign||function(e){for(var n=1;n<arguments.length;n++){var t=arguments[n];for(var r in t)Object.prototype.hasOwnProperty.call(t,r)&&(e[r]=t[r])}return e}).apply(this,arguments)}var a=Object(r.lazy)((function(){return t.e(/*! import() */3).then(t.bind(null,/*! ./PostItem */"./src/components/post/PostItem.js"))}));n.default=function(e){var n=e.posts,t=e.self,r=e.username,u=e.id;return s.a.createElement("div",null,n.map((function(e){var n=e.node;return s.a.createElement(a,o({key:n.id},n,{user_id:t?u:n.user.id,username:t?r:n.user.username,show_comment:!1}))})))}}}]);
+//# sourceMappingURL=1.main.js.map
