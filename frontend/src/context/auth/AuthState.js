50c31479ac600d034e72cb6249d60df0934766c5
Mati <mathiaskassaye8@gmail.com>
2020-09-04 15:06:48 +0300

    Added support for changing password, f*ed up the account/user page's style

--- frontend/src/context/auth/AuthState.js
+++ frontend/src/context/auth/AuthState.js
@@ -34,7 +34,7 @@ const AuthState = (props) => {
   const [refresh] = useMutation(REFRESH_TOKEN);
 
   const loggedIn = () => {
-    console.log(state.token);
+    console.log(state);
     if (state.token == null) {
       dispatch({
         type: LOGOUT,
