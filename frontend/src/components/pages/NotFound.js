4985295d4907be2d2fb2ec4549944fc7f20c6a70
Mati <mathiaskassaye8@gmail.com>
2020-09-05 12:45:24 +0300

    Added support for changing password, f*ed up the account/user page's style

--- frontend/src/components/pages/NotFound.js
+++ frontend/src/components/pages/NotFound.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 const NotFound = () => {
-  return ( <
-    >
-    <
-    h2 > 404 Not Found < /h2> <
-    p > The page you are looking
-    for does not exists. < /p> <
-    />
+  return (
+    <>
+      <h2>404 Not Found</h2>
+      <p>The page you are looking for does not exists.</p>
+    </>
   );
 };
 
