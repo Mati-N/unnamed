2fcf07e9fea66158306480030777528e4b183f5c
Mati <mathiaskassaye8@gmail.com>
2020-09-08 12:27:38 +0300

    Adding notificaions(not push)

--- frontend/src/components/post/Comment.js
+++ frontend/src/components/post/Comment.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Comment = ({ user: { username, id }, content }) => {
+const Comment = ({ user: { username, id: user_id }, content, id }) => {
   return (
     <div className="comment card" id={`${id}c`}>
       <div className="post-top card-top">
         <div className="post-info-top">
-          <Link to={`/user/${id}`} className="post-user">
+          <Link to={`/user/${user_id}`} className="post-user">
             {" "}
             {username}{" "}
           </Link>
--- static/frontend/9.main.js
+++ static/frontend/9.main.js
@@ -2,7 +2,7 @@
 /*!****************************************!*\
   !*** ./src/components/post/Comment.js ***!
   \****************************************/
-/*! exports provided: default */function(e,t,n){"use strict";n.r(t);var a=n(/*! react */"./node_modules/react/index.js"),o=n.n(a),r=n(/*! react-router-dom */"./node_modules/react-router-dom/esm/react-router-dom.js");t.default=function(e){var t=e.user,n=t.username,a=t.id,c=e.content;return o.a.createElement("div",{className:"comment card",id:"".concat(a,"c")},o.a.createElement("div",{className:"post-top card-top"},o.a.createElement("div",{className:"post-info-top"},o.a.createElement(r.Link,{to:"/user/".concat(a),className:"post-user"}," ",n," ")),o.a.createElement("pre",{className:"comment-content"},c)))}},"./src/components/post/Comments.js":
+/*! exports provided: default */function(e,t,n){"use strict";n.r(t);var a=n(/*! react */"./node_modules/react/index.js"),o=n.n(a),r=n(/*! react-router-dom */"./node_modules/react-router-dom/esm/react-router-dom.js");t.default=function(e){var t=e.user,n=t.username,a=t.id,c=e.content,s=e.id;return o.a.createElement("div",{className:"comment card",id:"".concat(s,"c")},o.a.createElement("div",{className:"post-top card-top"},o.a.createElement("div",{className:"post-info-top"},o.a.createElement(r.Link,{to:"/user/".concat(a),className:"post-user"}," ",n," ")),o.a.createElement("pre",{className:"comment-content"},c)))}},"./src/components/post/Comments.js":
 /*!*****************************************!*\
   !*** ./src/components/post/Comments.js ***!
   \*****************************************/
