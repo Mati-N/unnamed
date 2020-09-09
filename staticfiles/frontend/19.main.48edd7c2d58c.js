f939601812b8cf6089efbff12e33255d73161db7
Mati <mathiaskassaye8@gmail.com>
2020-09-09 10:28:16 +0300

    Adding notificaions(not push) and for viewing posts of people the logged in user follows

--- staticfiles/frontend/19.main.48edd7c2d58c.js
+++
@@ -1,6 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{"./src/components/pages/NotFound.js":
-/*!******************************************!*\
-  !*** ./src/components/pages/NotFound.js ***!
-  \******************************************/
-/*! exports provided: default */function(e,n,o){"use strict";o.r(n);var t=o(/*! react */"./node_modules/react/index.js"),a=o.n(t);n.default=function(){return a.a.createElement(a.a.Fragment,null,a.a.createElement("h2",null,"404 Not Found"),a.a.createElement("p",null,"The page you are looking for does not exists."))}}}]);
-//# sourceMappingURL=19.main.js.map
